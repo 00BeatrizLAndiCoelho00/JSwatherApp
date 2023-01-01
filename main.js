954323b7fde253060f60d0363a55cb670905d7fe
BeatrizLandi <andressa.fideliscoelho@gmail.com>
2023-01-01 17:31:41 -0300

    coletas de dados pronta

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import "./style.css"
 import "./heather.css"
 import "./daycard.css"
 import "./daysection.css"
+
+import { getWeather } from "./weather"
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+    data=> {
+        console.log(data)
+    }
+ ) 
